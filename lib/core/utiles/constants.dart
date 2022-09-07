14dc1c84753b293a42a5c05d5311f6e3303ba411
Samar <sam_abdulaziz@yahoo.com>
2022-09-07 20:58:03 +0200

    with no key

--- lib/core/utiles/constants.dart
+++ lib/core/utiles/constants.dart
@@ -1,6 +1,5 @@
 class AppConstants{
   static const String baseUrl='https://api.openweathermap.org/data/2.5';
-  static const String apiKey='148ed91fbbb09faf739bdfb485d07279';
   static  String getWeatherByCityNamePath(String cityName)=>'$baseUrl/weather?q=$cityName&units=metric&appid=$apiKey';
   static  String getWeatherDetailsByCityNamePath(String cityName)=>'$baseUrl/forecast?q=$cityName&units=metric&appid=$apiKey';
 }
