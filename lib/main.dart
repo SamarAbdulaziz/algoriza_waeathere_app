b584511fb1ef2ab8ffb78cf5790de1ddda137d05
Samar <sam_abdulaziz@yahoo.com>
2022-09-10 14:59:05 +0200

    branch merged into master

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/core/services/new_service_locator.dart';
-import 'features/weather/presentaion/controllers/bloc_observer.dart';
+import 'package:weather/features/weather/presentaion/controllers/bloc_observer.dart';
 import 'my_app.dart';
 
 void main() async {
