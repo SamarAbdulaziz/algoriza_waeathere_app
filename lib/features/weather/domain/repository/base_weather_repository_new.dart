36aec24a04b1603ec30b895781d05a4f5f9640d6
Samar <sam_abdulaziz@yahoo.com>
2022-09-10 14:39:54 +0200

    master is free again

--- lib/features/weather/domain/repository/base_weather_repository_new.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:weather/core/errors/failure.dart';
-import 'package:weather/features/weather/domain/entities/new_weather.dart';
-
-abstract class BaseWeatherRepositoryNew{
-  Future<Either<Failure, NewWeather>>getWeatherByCityName(String cityName);
-  // Future<Either<Failure, NewWeather>>getWeatherByCoordinates(double lat,double long);
-}
